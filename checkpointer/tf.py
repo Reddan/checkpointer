55b5cd16bc233d19ab270afbd1b5233cc839e0e5
Hampus Hallman <me@hampushallman.com>
2018-03-18 15:08:57 +0100

    updated PickleableTf

--- checkpointer/tf.py
+++ checkpointer/tf.py
@@ -1,6 +1,6 @@
-from .function_body import get_function_hash
-from relib import get_tmp_directory, DirectoryBytesIO
 from functools import partial
+from relib import DirectoryBytesIO
+from .function_body import get_function_hash
 
 class PickleableTf:
   def __init__(self, get_model_funcs, model_funcs_names=None, bytes=None):
@@ -18,30 +18,29 @@ class PickleableTf:
     self.bytes = bytes
 
     for func_name in model_funcs_names:
-      compute = partial(self.compute, func_name)
+      compute = partial(self._compute, func_name)
       setattr(self, func_name, compute)
 
-  def compute(self, func_name, *args, **kwargs):
+  def _compute(self, func_name, *args, **kwargs):
     import tensorflow as tf
 
-    load_tmp_directory = get_tmp_directory()
-    save_tmp_directory = get_tmp_directory()
-    file_name = 'tf.ckpt'
-
+    ckpt_file_name = 'tf.ckpt'
     tf.reset_default_graph()
     model_funcs = self.get_model_funcs()
     saver = tf.train.Saver()
 
     with tf.Session() as sess:
       if self.bytes:
-        self.bytes.unpack(load_tmp_directory)
-        saver.restore(sess, load_tmp_directory + file_name)
+        self.bytes.unpack(lambda tmp_directory: \
+          saver.restore(sess, tmp_directory + ckpt_file_name)
+        )
       else:
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
 
       result = model_funcs[func_name](sess, *args, **kwargs)
-      saver.save(sess, save_tmp_directory + file_name)
-      bytes = DirectoryBytesIO(save_tmp_directory)
+      bytes = DirectoryBytesIO(lambda tmp_directory: \
+        saver.save(sess, tmp_directory + ckpt_file_name)
+      )
       model = PickleableTf(self.get_model_funcs, self.model_funcs_names, bytes)
       return model, result
