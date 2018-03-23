0ed1d3a27b283f8513e2ff0f93ed8b1e8015f620
Hampus Hallman <me@hampushallman.com>
2018-03-23 13:32:47 +0100

    tf fix

--- checkpointer/tf.py
+++ checkpointer/tf.py
@@ -2,15 +2,20 @@ from functools import partial
 from relib import DirectoryBytesIO
 from .function_body import get_function_hash
 
+def ensure_list(val):
+  if isinstance(val, (tuple, list)):
+    return list(val)
+  else:
+    return [val]
+
 class PickleableTf:
   def __init__(self, get_model_funcs, model_funcs_names=None, bytes=None):
     if model_funcs_names == None:
       import tensorflow as tf
-      try:
-        tf.reset_default_graph()
-      except AssertionError:
-        pass
-      model_funcs_names = sorted(get_model_funcs().keys())
+
+      with tf.Graph().as_default():
+        model_funcs = ensure_list(get_model_funcs())
+        model_funcs_names = [func.__name__ for func in model_funcs]
 
     self.model_funcs_names = model_funcs_names
     self.get_model_funcs = get_model_funcs
@@ -24,23 +29,27 @@ class PickleableTf:
   def _compute(self, func_name, *args, **kwargs):
     import tensorflow as tf
 
-    ckpt_file_name = 'tf.ckpt'
-    tf.reset_default_graph()
-    model_funcs = self.get_model_funcs()
-    saver = tf.train.Saver()
+    with tf.Graph().as_default():
+      model_funcs = {
+        func.__name__: func
+        for func in ensure_list(self.get_model_funcs())
+      }
 
-    with tf.Session() as sess:
-      if self.bytes:
-        self.bytes.unpack(lambda tmp_directory: \
-          saver.restore(sess, tmp_directory + ckpt_file_name)
+      ckpt_file_name = 'tf.ckpt'
+      saver = tf.train.Saver()
+
+      with tf.Session() as sess:
+        if self.bytes:
+          self.bytes.unpack(lambda tmp_directory: \
+            saver.restore(sess, tmp_directory + ckpt_file_name)
+          )
+        else:
+          init_op = tf.global_variables_initializer()
+          sess.run(init_op)
+
+        result = model_funcs[func_name](sess, *args, **kwargs)
+        bytes = DirectoryBytesIO(lambda tmp_directory: \
+          saver.save(sess, tmp_directory + ckpt_file_name)
         )
-      else:
-        init_op = tf.global_variables_initializer()
-        sess.run(init_op)
-
-      result = model_funcs[func_name](sess, *args, **kwargs)
-      bytes = DirectoryBytesIO(lambda tmp_directory: \
-        saver.save(sess, tmp_directory + ckpt_file_name)
-      )
-      model = PickleableTf(self.get_model_funcs, self.model_funcs_names, bytes)
-      return model, result
+        model = PickleableTf(self.get_model_funcs, self.model_funcs_names, bytes)
+        return model, result
