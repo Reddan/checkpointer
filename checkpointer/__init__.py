b2832d421940e7c4f174355425f278b305fa9563
Hampus Hallman <me@hampushallman.com>
2018-04-27 10:36:05 +0200

    removed PickleableTf

--- checkpointer/__init__.py
+++ checkpointer/__init__.py
@@ -1,4 +1,3 @@
 from .checkpoint import checkpoint, read_only
 from .storage import store_on_demand, read_from_store
 from .function_body import get_function_hash, get_function_dir_path, get_function_project_dir_path
-from .tf import PickleableTf
