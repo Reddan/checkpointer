26adc972e71cdbc054a50ca1f4bf1b8115cd1a98
Hampus Hallman <me@hampushallman.com>
2018-09-19 22:57:56 +0200

    added environment variable for verbosity

--- checkpointer/env.py
+++ checkpointer/env.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 default_dir = str(Path.home()) + '/.checkpoints'
 storage_dir = os.environ.get('CHECKPOINTS_DIR', default_dir) + '/'
+verbosity = os.environ.get('CHECKPOINTER_VERBOSITY', 'NORMAL')
 
 mongo_url = os.environ.get('MONGO_URL', 'mongodb://localhost')
 mongo_db = os.environ.get('MONGO_CHECKPOINTS_DB', 'checkpoints')
