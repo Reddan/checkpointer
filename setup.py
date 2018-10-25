73716a8c71fb743baa7e294b02c809af0f329a07
Hampus Hallman <me@hampushallman.com>
2018-10-25 14:48:08 +0200

    bumped version number to 0.3.0

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from setuptools import setup
 setup(
   name='checkpointer',
   packages=['checkpointer', 'checkpointer.storages'],
-  version='0.2.0',
+  version='0.3.0',
   author='Hampus Hallman',
   author_email='me@hampushallman.com',
   url='https://github.com/Reddan/checkpointer',
   license='MIT',
   install_requires=[
     'relib',
-    'numpy',
     'termcolor'
   ],
   python_requires='~=3.5',
