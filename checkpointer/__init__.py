69dbb17ed4bc0c0e155068577ac2844bd9aa0900
Hampus Hallman <me@hampushallman.com>
2018-10-29 10:23:26 +0100

    restored environment variable API

--- checkpointer/__init__.py
+++ checkpointer/__init__.py
@@ -1,5 +1,10 @@
-from .checkpoint import create_checkpointer, read_only
+import os
+
+from .checkpoint import create_checkpointer, read_only, default_dir
 from .storage import store_on_demand, read_from_store
 from .function_body import get_function_hash, get_function_dir_path, get_function_project_dir_path
 
-checkpoint = create_checkpointer()
+storage_dir = os.environ.get('CHECKPOINTS_DIR', default_dir)
+verbosity = int(os.environ.get('CHECKPOINTS_VERBOSITY', '1'))
+
+checkpoint = create_checkpointer(storage_dir, verbosity)
