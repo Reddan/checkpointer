e3bca48548b9daf0de58a26abc0e0bc808a2b5bf
Hampus Hallman <me@hampushallman.com>
2019-01-07 21:59:02 +0100

    simplified get_function_hash

--- checkpointer/function_body.py
+++ checkpointer/function_body.py
@@ -1,15 +1,11 @@
 import os
 import inspect
 from types import FunctionType, CodeType
-from relib import imports, hashing, raypipe
+from relib.raypipe import raypipe
+import relib.hashing as hashing
+from pathlib import Path
 
-def get_function_dir_path(func):
-  return os.path.dirname(os.path.abspath(func.__code__.co_filename))
-
-def get_function_project_dir_path(func_dir_path):
-  return imports.find_parent_dir_containing(func_dir_path, '.git') \
-    or imports.find_parent_dir_containing(func_dir_path, '__init__.py') \
-    or os.getcwd()
+cwd = Path(os.getcwd())
 
 def get_function_body(func):
   # TODO: Strip comments
@@ -25,7 +21,7 @@ def get_code_children(__code__):
     .do(lambda children: list(__code__.co_names) + children) \
     .compute(__code__.co_consts)
 
-def get_func_children(func, func_proj_path, func_by_wrapper={}, neighbor_funcs=[]):
+def get_func_children(func, func_by_wrapper={}, neighbor_funcs=[]):
   def get_candidate_func(co_name):
     try:
       candidate_func = func.__globals__.get(co_name, None)
@@ -35,11 +31,9 @@ def get_func_children(func, func_proj_path, func_by_wrapper={}, neighbor_funcs=[
       return None
 
   def clear_candidate(candidate_func):
-    if isinstance(candidate_func, FunctionType):
-      if candidate_func not in neighbor_funcs:
-        func_dir_path = get_function_dir_path(candidate_func)
-        return func_proj_path in func_dir_path
-    return False
+    return isinstance(candidate_func, FunctionType) \
+      and candidate_func not in neighbor_funcs \
+      and cwd in Path(inspect.getfile(candidate_func)).parents
 
   code_children = get_code_children(func.__code__)
 
@@ -50,7 +44,7 @@ def get_func_children(func, func_proj_path, func_by_wrapper={}, neighbor_funcs=[
 
   funcs = raypipe \
     .flat_map(lambda child_func: \
-      get_func_children(child_func, func_proj_path, func_by_wrapper, func_children)
+      get_func_children(child_func, func_by_wrapper, func_children)
     ) \
     .do(lambda grand_children: [func] + grand_children) \
     .sort_distinct(lambda func: func.__name__) \
@@ -59,9 +53,7 @@ def get_func_children(func, func_proj_path, func_by_wrapper={}, neighbor_funcs=[
   return funcs
 
 def get_function_hash(func, func_by_wrapper={}):
-  func_dir_path = get_function_dir_path(func)
-  func_proj_path = get_function_project_dir_path(func_dir_path)
-  funcs = [func] + get_func_children(func, func_proj_path, func_by_wrapper)
+  funcs = [func] + get_func_children(func, func_by_wrapper)
   function_bodies = raypipe.map(get_function_body).compute(funcs)
   function_bodies_hash = hashing.hash(function_bodies)
   return function_bodies_hash
