19b2fd696ea057a35f0bceda7c5f67c48624c3f2
Hampus <hampus@hallman.eu>
2019-03-23 15:26:09 +0100

    removed func_by_wrapper

--- checkpointer/utils.py
+++ checkpointer/utils.py
@@ -1,9 +1,16 @@
 import os
+import errno
 
 def ensure_dir(directory):
   if not os.path.exists(directory):
     try:
       os.makedirs(directory)
     except OSError as e:
-      if e.errno != os.errno.EEXIST:
+      if e.errno != errno.EEXIST:
         raise
+
+def unwrap_func(func):
+  if hasattr(func, '__wrapped__'):
+    return unwrap_func(func.__wrapped__)
+  else:
+    return func
