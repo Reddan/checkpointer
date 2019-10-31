bf500005871cb64b44cb49f2f30ba6976e7922e9
Hampus Hallman <hampus@hallman.eu>
2019-10-31 15:58:06 +0100

    support for asynchronous functions

--- checkpointer/storage.py
+++ checkpointer/storage.py
@@ -8,13 +8,14 @@ storages = {
 }
 
 initialized_storages = set()
-invoke_level = {'val': -1}
 
-def log(color, title, invoke_level, name):
-  title_log = colored(title, 'grey', 'on_' + color)
-  invoke_level_log = (' ' * min(1, invoke_level)) + ('──' * invoke_level)
-  rest_log = colored(invoke_level_log + ' ' + name, color)
-  print(title_log + rest_log)
+def create_logger(should_log):
+  def log(color, title, text):
+    if should_log:
+      title_log = colored(f' {title} ', 'grey', 'on_' + color)
+      rest_log = colored(text, color)
+      print(title_log + ' ' + rest_log)
+  return log
 
 def get_storage(storage):
   if type(storage) == str:
@@ -25,30 +26,27 @@ def get_storage(storage):
     initialized_storages.add(storage)
   return storage
 
-def store_on_demand(func, name, config, storage='pickle', force=False, should_expire=None):
-  try:
-    invoke_level['val'] += 1
-    storage = get_storage(storage)
-    do_print = storage != memory_storage and config.verbosity != 0
-    refresh = force \
-      or storage.get_is_expired(config, name) \
-      or (should_expire and storage.should_expire(config, name, should_expire))
+async def store_on_demand(get_data, name, config, force=False, should_expire=None):
+  storage = get_storage(config.format)
+  should_log = storage != memory_storage and config.verbosity != 0
+  log = create_logger(should_log)
+  refresh = force \
+    or storage.get_is_expired(config, name) \
+    or (should_expire and storage.should_expire(config, name, should_expire))
 
-    if refresh:
-      if do_print: log('blue', ' MEMORIZING ', invoke_level['val'], name)
-      data = func()
-      return storage.store_data(config, name, data)
-    else:
-      try:
-        data = storage.load_data(config, name)
-        if do_print: log('green', ' REMEMBERED ', invoke_level['val'], name)
-        return data
-      except (EOFError, FileNotFoundError):
-        storage.delete_data(config, name)
-        print(name + ' corrupt, removing')
-        return store_on_demand(func, name, config, storage, force, should_expire)
-  finally:
-    invoke_level['val'] -= 1
+  if refresh:
+    log('blue', 'MEMORIZING', name)
+    data = await get_data()
+    return storage.store_data(config, name, data)
+  else:
+    try:
+      data = storage.load_data(config, name)
+      log('green', 'REMEMBERED', name)
+      return data
+    except (EOFError, FileNotFoundError):
+      log('yellow', 'CORRUPTED', name)
+      storage.delete_data(config, name)
+      return await store_on_demand(get_data, name, config, force, should_expire)
 
 def read_from_store(name, config, storage='pickle'):
   storage = get_storage(storage)
