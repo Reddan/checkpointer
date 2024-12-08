091f916c180b77fbd600b1a003c08c97d9cb8ef2
Hampus Hallman <hampus@hallman.eu>
2024-12-08 12:38:45 +0100

    2.2.0 - Separate directories for each fn hash, storage cleanup method

--- checkpointer/storages/memory_storage.py
+++ checkpointer/storages/memory_storage.py
@@ -3,23 +3,38 @@ from pathlib import Path
 from datetime import datetime
 from ..types import Storage
 
-item_map: dict[str, tuple[datetime, Any]] = {}
+item_map: dict[Path, dict[str, tuple[datetime, Any]]] = {}
 
 class MemoryStorage(Storage):
+  def get_dict(self):
+    return item_map.setdefault(self.checkpointer.root_path / self.checkpoint_fn.fn_subdir, {})
+
   def get_short_path(self, path: Path):
-    return str(path.relative_to(self.checkpointer.root_path))
+    return path.parts[-1]
 
   def exists(self, path):
-    return self.get_short_path(path) in item_map
+    return self.get_short_path(path) in self.get_dict()
 
   def checkpoint_date(self, path):
-    return item_map[self.get_short_path(path)][0]
+    return self.get_dict()[self.get_short_path(path)][0]
 
   def store(self, path, data):
-    item_map[self.get_short_path(path)] = (datetime.now(), data)
+    self.get_dict()[self.get_short_path(path)] = (datetime.now(), data)
 
   def load(self, path):
-    return item_map[self.get_short_path(path)][1]
+    return self.get_dict()[self.get_short_path(path)][1]
 
   def delete(self, path):
-    del item_map[self.get_short_path(path)]
+    del self.get_dict()[self.get_short_path(path)]
+
+  def cleanup(self, invalidated=True, expired=True):
+    key = self.checkpointer.root_path / self.checkpoint_fn.fn_subdir
+    parent = key.parent
+    for k, d in list(item_map.items()):
+      if k == key:
+        if expired and self.checkpointer.should_expire:
+          for callid, (date, _) in list(d.items()):
+            if self.checkpointer.should_expire(date):
+              del d[callid]
+      elif invalidated and k.parent == parent:
+        del item_map[k]
--- checkpointer/storages/pickle_storage.py
+++ checkpointer/storages/pickle_storage.py
@@ -1,4 +1,5 @@
 import pickle
+import shutil
 from pathlib import Path
 from datetime import datetime
 from ..types import Storage
@@ -29,3 +30,21 @@ class PickleStorage(Storage):
       get_path(path).unlink()
     except FileNotFoundError:
       pass
+
+  def cleanup(self, invalidated=True, expired=True):
+    version_path = self.checkpointer.root_path.resolve() / self.checkpoint_fn.fn_subdir
+    fn_path = version_path.parent
+    if invalidated:
+      old_dirs = [path for path in fn_path.iterdir() if path.is_dir() and path != version_path]
+      for path in old_dirs:
+        path.parent
+        shutil.rmtree(path)
+      print(f"Removed {len(old_dirs)} invalidated directories for {self.checkpoint_fn.__qualname__}")
+    if expired and self.checkpointer.should_expire:
+      count = 0
+      for pkl_path in fn_path.rglob("*.pkl"):
+        path = pkl_path.with_suffix("")
+        if self.checkpointer.should_expire(self.checkpoint_date(path)):
+          count += 1
+          self.delete(path)
+      print(f"Removed {count} expired checkpoints for {self.checkpoint_fn.__qualname__}")
